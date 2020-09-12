fe155ffd2440c1f2ab9a1d92977619ff95a4cceb
ufuk <ufuk.uysl5@gmail.com>
2020-09-12 23:01:15 +0200

    insert many data to collection

--- mongodb.js
+++ mongodb.js
@@ -13,7 +13,7 @@ MongoClient.connect(
       return console.log("unable to connect to database!"); //we use return that because stop execution
     }
     const db = client.db(databaseName); // for database manipulation
-
+    /* 
     db.collection("users").insertOne(
       {
         name: "Ufuk",
@@ -24,6 +24,24 @@ MongoClient.connect(
           return console.log("unable to insert user");
         }
 
+        console.log(result.ops); // equal to what we add
+      }
+    ); */
+    db.collection("users").insertMany(
+      [
+        {
+          name: "uysal",
+          age: 23,
+        },
+        {
+          name: "john",
+          age: 22,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("unable to insert user");
+        }
         console.log(result.ops); // equal to what we add
       }
     );
