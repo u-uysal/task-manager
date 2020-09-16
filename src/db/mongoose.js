6e3a5c26525be16b394a5fb2a3b4a5ddb3f932ab
ufuk <ufuk.uysl5@gmail.com>
2020-09-16 13:46:15 +0200

    password validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -15,6 +15,17 @@ const User = mongoose.model("User", {
     required: true,
     trim: true,
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password can not contain 'password' ");
+      }
+    },
+  },
   email: {
     type: String,
     required: true,
