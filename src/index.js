552308887194e9760f89ac1094b1531d6d794ce8
ufuk <ufuk.uysl5@gmail.com>
2020-09-17 09:46:02 +0200

    get unique user by using params

--- src/index.js
+++ src/index.js
@@ -32,6 +32,20 @@ app.get("/users", (req, res) => {
     });
 });
 
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(() => {
+      return res.status(500).send();
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
 
