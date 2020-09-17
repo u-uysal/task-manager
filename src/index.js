69c3fddf1493d355bc986cea08e2d1e78b116281
ufuk <ufuk.uysl5@gmail.com>
2020-09-17 09:57:53 +0200

    get unique task

--- src/index.js
+++ src/index.js
@@ -68,6 +68,20 @@ app.get("/tasks", (req, res) => {
     });
 });
 
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((e) => {
+      return res.status(500).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
