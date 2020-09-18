372ab066f5607721fcf560dcdd7f90213fa086d4
ufuk <ufuk.uysl5@gmail.com>
2020-09-18 11:28:56 +0200

    delete user by using id

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ app.patch("/users/:id", async (req, res) => {
   });
 
   if (!isUptade) {
-    return res.status(400).send("error : Invalid updates");
+    return res.status(400).send({ error: "Invalid updates" });
   }
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
@@ -72,6 +72,18 @@ app.patch("/users/:id", async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
@@ -104,6 +116,31 @@ app.get("/tasks/:id", async (req, res) => {
     res.send(task);
   } catch (error) {
     return res.status(500).send(error);
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "completed"]; //user can change only this info
+
+  const isUptade = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isUptade) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    return res.status(400).send(error);
   }
 });
 
