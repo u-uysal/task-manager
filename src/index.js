57303f0e4a93dc30c418cf6d60f0afa69cb0007a
ufuk <ufuk.uysl5@gmail.com>
2020-09-18 11:29:28 +0200

    delete task by using id

--- src/index.js
+++ src/index.js
@@ -144,6 +144,19 @@ app.patch("/tasks/:id", async (req, res) => {
   }
 });
 
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
