fcdf1724464739c23171c4b5a330dffdaebe934e
ufuk <ufuk.uysl5@gmail.com>
2020-09-18 15:02:09 +0200

    use middleware for tasks router

--- src/routers/task.js
+++ src/routers/task.js
@@ -50,10 +50,10 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(req.params.id);
+    updates.forEach((item) => (task[item] = req.body[item]));
+
+    await task.save();
 
     if (!task) {
       return res.status(404).send();
