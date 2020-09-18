2cb9453e63885e7e599fd2250e7dd5f247d42707
ufuk <ufuk.uysl5@gmail.com>
2020-09-18 22:03:48 +0200

    create token to auth

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,6 +42,12 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "randomtext");
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
